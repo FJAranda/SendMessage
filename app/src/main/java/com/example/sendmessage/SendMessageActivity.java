4f0aa7aec00ec5fd97c0c7b887d762bb3cf77128
Javier Aranda <javierarandacaro@gmail.com>
2017-09-28 09:42:32 +0200

    Terminado, o no(?) Muajajajajaja!

--- app/src/main/java/com/example/sendmessage/SendMessageActivity.java
+++ app/src/main/java/com/example/sendmessage/SendMessageActivity.java
@@ -4,20 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class SendMessageActivity extends AppCompatActivity {
+public class SendMessageActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText etMensaje;
+    private EditText etUsuario;
+    private Button btnOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
         etMensaje = (EditText)findViewById(R.id.etMensaje);
+        etUsuario = (EditText)findViewById(R.id.etUsuario);
+        btnOk = (Button)findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(this);
     }
 
-    public void getOnCLick(View view){
+    @Override
+    public void onClick(View view) {
         Bundle bundle = new Bundle();
-        bundle.putString("Mensaje", etMensaje.getText().toString());
+        bundle.putString("mensaje", etMensaje.getText().toString());
+        bundle.putString("usuario", etUsuario.getText().toString());
         Intent intent = new Intent(SendMessageActivity.this,ViewMessageActivity.class);
         intent.putExtras(bundle);
         startActivity(intent);
--- app/src/main/java/com/example/sendmessage/ViewMessageActivity.java
+++ app/src/main/java/com/example/sendmessage/ViewMessageActivity.java
@@ -1,13 +1,21 @@
 package com.example.sendmessage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewMessageActivity extends AppCompatActivity {
-
+    private TextView tvViewUser;
+    private TextView tvViewMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_message);
+        tvViewUser = (TextView)findViewById(R.id.tvViewUser);
+        String viewUser = String.format(getResources().getString(R.string.tvViewUser), getIntent().getExtras().getString("usuario"));
+        tvViewMessage = (TextView)findViewById(R.id.tvViewMessage);
+        tvViewMessage.setText(getIntent().getExtras().getString("mensaje"));
+        tvViewUser.setText(viewUser);
     }
 }
